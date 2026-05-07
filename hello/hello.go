ae4761b46b8a3d4a0f7ed3f20c1d4bc984665722
Terry Huang <th04e22@gmail.com>
2026-05-07 22:30:34 +0800

    [Add] first commit

---
+++ hello/hello.go
@@ -0,0 +1,24 @@
+package hello
+
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	}
+
+	return prefix + name
+}
