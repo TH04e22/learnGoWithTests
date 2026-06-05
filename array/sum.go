50f1e01deddbb19cebd0e421db98eb136dcdd6e1
Terry Huang <th04e22@gmail.com>
2026-06-05 10:16:46 +0800

    [Add] Reduce function

--- array/sum.go
+++ array/sum.go
@@ -1,13 +1,9 @@
 package array
 
 func Sum(numbers []int) int {
-	sum := 0
+	add := func(acc, x int) int { return acc + x }
 
-	for _, number := range numbers {
-		sum += number
-	}
-
-	return sum
+	return Reduce(numbers, add, 0)
 }
 
 func SumAll(numbersToSum ...[]int) []int {
@@ -22,16 +18,23 @@ func SumAll(numbersToSum ...[]int) []int {
 }
 
 func SumAllTails(numbersToSum ...[]int) []int {
-	var sums []int
-
-	for _, numbers := range numbersToSum {
-		if len(numbers) == 0 {
-			sums = append(sums, 0)
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
 		} else {
-			tail := numbers[1:]
-			sums = append(sums, Sum(tail))
+			tail := x[1:]
+			return append(acc, Sum(tail))
 		}
 	}
 
-	return sums
+	return Reduce(numbersToSum, sumTail, []int{})
+}
+
+func Reduce[A any](collection []A, f func(A, A) A, initialValue A) A {
+	var result = initialValue
+	for _, x := range collection {
+		result = f(result, x)
+	}
+
+	return result
 }
